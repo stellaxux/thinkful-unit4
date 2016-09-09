753ff0e0056a7feca1721394ba18b437c7741641
Stella Xu <xinxu0824@gmail.com>
2016-09-08 20:23:17 -0400

    Unit4Lesson4

--- knn.py
+++ knn.py
@@ -15,29 +15,32 @@ from sklearn.neighbors import KNeighborsClassifier
 cols = ['sepal_length','sepal_width','petal_length','petal_width','class']
 data = pd.read_csv('iris.data.csv', names=cols)
 
-groups = data.groupby('class')
-colors = {'Iris-setosa':'red', 'Iris-versicolor':'blue', 'Iris-virginica':'green'}
+data['class'].replace('Iris-setosa',1, inplace = True)
+data['class'].replace('Iris-versicolor', 2, inplace = True)
+data['class'].replace('Iris-virginica', 3, inplace = True)
 
-for name, group in groups:
-    plt.scatter(group.sepal_length, group.sepal_width, c=colors[name], label=name)
+plt.scatter(data.sepal_length, data.sepal_width, c=data['class'])
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
-plt.legend()
 plt.show()
 
 length = random.uniform(4, 8)
 width = random.uniform(1, 5)
 
+# self-written knn
 for index, row in data.iterrows():
     data.loc[index, 'distance'] = math.sqrt((row['sepal_length']-length)**2 + (row['sepal_width']-width)**2)
 nearest = data.nsmallest(10, 'distance')
 nearest['class'].value_counts().idxmax()
+#'Iris-versicolor'
+
 
 # apply KNN from sklearn
 knn = KNeighborsClassifier(10)
-X = data[['sepal_length','sepal_width']].as_matrix()
+X = data[[0,1]].values
 y = data['class'].as_matrix()
-knn.fit(X,y)
+predicted = knn.fit(X,y).predict(X)
+accuracy = knn.score(X, y)
 
 test = np.asarray([length, width])
 test = test.reshape(1, 2) 
